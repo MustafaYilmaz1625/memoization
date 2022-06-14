7f0b6625b2d2051144a6aada0ee06f9de933718d
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-06-14 22:41:05 +0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import Header from "./components/Header";
 
 function App() {
   const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
+
+  const data = useMemo(() => {
+    return calculateObject();
+  }, []);
 
   return (
     <div className="App">
-      <Header number={number < 5 ? 0 : number} />
+      <Header data={data} />
       <hr />
 
       <h1>{number}</h1>
       <button onClick={() => setNumber(number + 1)}>Click</button>
+      <br />
+      <br />
+      <input value={text} onChange={(target) => setText(target.value)} />
     </div>
   );
+}
+
+function calculateObject() {
+  console.log("Calculating Object...");
+  for (let i = 0; i < 1000000000; i++) console.log("Calculating completed!!");
+  return { name: "mustafa" };
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function Header({ number }) {
-  return <div>Header- {number}</div>;
+function Header({ number, data }) {
+  console.log("Header");
+  return (
+    <div>
+      Header- {number}
+      <br />
+      <br />
+      <code>{JSON.stringify(data)}</code>
+    </div>
+  );
 }
 
 export default React.memo(Header);
